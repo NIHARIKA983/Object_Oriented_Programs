461daf05d4eb42dea9f511febfec52c0e171481b
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-25 16:56:03 +0530

    Added:Stock Account Management

---
+++ src/main/java/com/bridgelabz/StockManagementMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class StockManagementMain {
+    public static void main(String[] args){
+        StockManagement stockManagement = new StockManagement();
+        stockManagement.getInputFromUser();
+    }
+}
